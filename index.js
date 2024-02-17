7a100b0ff9f493e99942f00f70391b2e3aa9a5e6
N P Aishwarya <aishwaryanaik3138@gmail.com>
2024-02-17 20:41:14 +0530

    done

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
-import express from 'express'; 
-import bodyParser from "body-parser";
+const express = require('express');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
 
+//middle ware
+app.use(express.json());
+app.use('/auth',authRoutes);
+app.use('/blogs',blogRoutes);
 
-//middleware
-//app.use(express.json())//json data
-
-//third party middleware
-//app.use(bodyParser.json())
-//costome middleware
-
-const log=(req,res,next)=>{
-    console.log(`midlleware 1 executed`);
-    next()
-}
-
-const log1=(req,res,next)=>{
-    res.send(`middleware 2 is executed`);
-}
-app.use(log)
-app.use(log1)
-
-
-let users=[];
-
-
-const port = 1250;
-
-app.get('/products/:productId',(req,res) => {
-    console.log(req.params.productId);
-    let productId=req.params.productId;
-    res.send(productId)
-
-})
-
-app.post("/users",(req,res)=>{
-console.log(req.boby);
-users.push(req.body)
-res.send(users);
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port `); 
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,() =>{
+    console.log(`server is running on port ${PORT}`);
 });
